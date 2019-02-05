493ee813b3aacb52d4ffd10413e5a3ca78489007
evatd <eva.tkavc@gmail.com>
2019-02-05 17:21:55 +0000

    add comments to the propType util, clean up mdx

--- src/utilities/proptypes-to-object/index.js
+++ src/utilities/proptypes-to-object/index.js
@@ -2,29 +2,41 @@
 import PropTypes from "prop-types";
 
 // helper method to determine whether a propType method is a particular PropType validator
-const isCorrectPropType = (method, propType) =>
-  method === PropTypes[propType] || method === PropTypes[propType].isRequired;
+// i.e. string, node, or bool
+const isCorrectPropType = (method, propType) => {
+  return (
+    method === PropTypes[propType] || method === PropTypes[propType].isRequired
+  );
+};
 
 // helper method to determine whether a prop is required or not - if it is required,
 // it'll be equal to the .isRequired method on the propType.
-const isPropTypeRequired = (method, propType) =>
-  method === PropTypes[propType].isRequired;
+// propType here is the current object type only, e.g. string
+const isPropTypeRequired = (method, propType) => {
+  return method === PropTypes[propType].isRequired;
+};
 
 // method to determine what PropType a particular prop is. It does this by taking
 // the PropType validator of a particular prop, and running it against each of the
 // PropType validators set in the propTypes array. If a match is found, we return
 // an object of the prop type, and whether it is required or not.
 const getPropInfo = method => {
-  // For our example, we've only included these three prop types in the array,
-  // as that is what the Badge takes. But as a component library grows, this
-  // would need to be updated whenver you add a new component.
+  // Array needs to be updated whenever we add a new propType you add a new component
   const propTypes = ["string", "node", "bool"];
 
   return propTypes.reduce((obj, curr) => {
+    // Returns true for the PropType validators that match our propTypes (e.g. string)
+    // And false for those that don't match (e.g. node, bool)
     const isCorrect = isCorrectPropType(method, curr);
+
+    // Our obj is {type: "string", required: true}
+    // If type is string, we return an object of prop type
     if (obj.type) {
       return obj;
     }
+
+    // If required - we first assign current value to object type
+    // thus making obj.type a string
     if (isCorrect) {
       obj.type = curr;
       obj.required = isPropTypeRequired(method, curr);
@@ -33,11 +45,13 @@ const getPropInfo = method => {
   }, {});
 };
 
-// this takes all the propTypes (in our example - the value of Badge.propTypes)
-// and loops through each, and returns a human-readable object containing
+// Loops through propType, and returns a human-readable object containing
 // type and required status for each prop.
 const propTypesToObject = ({ propTypes }) => {
+  // Object.keys returns property name in an array, e.g. [title]
+  // We add the following arguments to reduce = object and current value, e.g. title
   let newProps = Object.keys(propTypes).reduce((obj, curr) => {
+    // Info returns an object with required and value key-value pairs, e.g. {type: "string", required: true}
     const info = getPropInfo(propTypes[curr]);
     return {
       ...obj,
