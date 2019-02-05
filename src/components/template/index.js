793762c43733fe94536fa66adf2ad8a6a24be30b
evatd <eva.tkavc@gmail.com>
2019-02-05 17:56:38 +0000

    add emotion to an mdx component - h1, demo

--- src/components/template/index.js
+++ src/components/template/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { css } from "@emotion/core";
+
 import Heading from "../heading";
 import Paragraph from "../paragraph";
 import List from "../list";
@@ -7,8 +9,19 @@ import Code from "../code";
 import ListItem from "../list-item";
 import Link from "../link";
 
+// example of using emotion,
+// update others, as fontsize isn't applied
 export default {
-  h1: props => <Heading {...props} as="h1" fontSize="40px" />,
+  h1: props => (
+    <Heading
+      {...props}
+      as="h1"
+      css={css`
+        font-size: 60px;
+        letter-spacing: 1px;
+      `}
+    />
+  ),
   h2: props => <Heading {...props} as="h2" fontSize="25px" />,
   h3: props => <Heading {...props} as="h3" fontSize="22px" />,
   h4: props => <Heading {...props} as="h4" fontSize="20px" />,
